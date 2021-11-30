ecba3a4520dd8a78cc4e923cd8d1251a157e1dfc
hamid <hbeigi57@gmail.com>
2021-11-30 16:42:32 +0330

    header done

--- assets/js/myJs.js
+++ assets/js/myJs.js
@@ -1,12 +1,36 @@
 $(document).ready(function () {
     $("body").click(function (e) { 
-        if($(".myDropdown").hasClass("myShow"))
-            $(".myDropdown").removeClass("myShow");
+        
+        $(".myDropdown").removeClass("myShow");
+        $("#small_screen_navbar").removeClass("nav-show");
     });
     $(".drop-toggle").click(function (e) { 
         e.stopPropagation();
         $(".myDropdown").toggleClass("myShow");
         
     });
-    
+    $("#store_click").click(function (e) { 
+        e.preventDefault();
+        e.stopPropagation();
+        $("#small_screen_mega_menu").addClass("mega-show");
+        
+    });
+    $("#closer").click(function (e) { 
+        e.preventDefault();
+        e.stopPropagation();
+        $("#small_screen_mega_menu").removeClass("mega-show");
+    });
+    $("#sth_lg_screen_tollger").click(function (e) { 
+        e.preventDefault();
+        e.stopPropagation();
+        $("#small_screen_navbar").addClass("nav-show");
+    });
+    $("#sm_sc_nav_closer").click(function (e) { 
+        e.preventDefault();
+        $("#small_screen_navbar").removeClass("nav-show");
+    });
+    $("#small_screen_navbar").click(function (e) { 
+        e.stopPropagation()
+        
+    });
 });
